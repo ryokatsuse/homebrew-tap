d2dccdd50851e6209b5f32903dd755080187017a
Ryo Katsuse <ryokatsu0719@gmail.com>
2026-01-20 14:23:30 +0900

    chore: bump dev-launcher to v0.3.0
    - TUI mode for real-time log viewing
    - remove/edit commands
    - README documentation

--- Formula/dev-launcher.rb
+++ Formula/dev-launcher.rb
@@ -1,13 +1,13 @@
 class DevLauncher < Formula
   desc "Development environment launcher for managing multiple projects"
   homepage "https://github.com/ryokatsuse/dev-launcher"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ryokatsuse/dev-launcher/releases/download/v0.2.0/dev-launcher-0.2.0-darwin-arm64.tar.gz"
-      sha256 "65529b31dc2cebf7a78e99d00a754b1639d4513e073dc6fb393031f3ccd2bdc2"
+      url "https://github.com/ryokatsuse/dev-launcher/releases/download/v0.3.0/dev-launcher-0.3.0-darwin-arm64.tar.gz"
+      sha256 "599ca64f23fc452a47e39e3868ddd458ffe9ae468c32608fa64933748df6e0e8"
     end
   end
 
